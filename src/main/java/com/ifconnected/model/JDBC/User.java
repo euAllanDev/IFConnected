3be354b1381e6b79f88ae34616cf4574b200080a
Allan <jorgeallandev@gamil.com>
2026-03-17 12:17:20 -0300

    teste hubfy

--- src/main/java/com/ifconnected/model/JDBC/User.java
+++ src/main/java/com/ifconnected/model/JDBC/User.java
@@ -3,6 +3,8 @@ package com.ifconnected.model.JDBC;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 
+// teste hubfy
+
 public class User implements java.io.Serializable {
 
     private static final long serialVersionUID = 1L;
